92d34c29c087db26938c205e82bd6c123978f2f2
Aleksei <akimov228aleksei@mail.ru>
2022-05-19 01:44:31 +0300

    Updated urls

--- src/management/backend/urls.py
+++ src/management/backend/urls.py
@@ -2,7 +2,7 @@
 
 from django.urls import path, include, re_path
 from rest_framework.routers import SimpleRouter
-from .views import department, employee, position, vacation
+from .views import department, employee, position, vacation, user_permissions
 
 router = SimpleRouter()
 router.register(r'department', department.DepartmentViewSet, basename='department')
@@ -14,4 +14,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('djoser.urls')),
     re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('permissions/', user_permissions.GetUserPermissions.as_view())
 ]
