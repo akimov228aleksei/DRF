d572df1903fb73a934e7be885c1a9518317fd29d
Aleksei <akimov228aleksei@mail.ru>
2022-06-06 00:07:37 +0300

    Updated models, makefile

--- src/management/backend/models/vacation.py
+++ src/management/backend/models/vacation.py
@@ -5,7 +5,7 @@ from django.db import models
 class Vacation(models.Model):
     """Class containing fields and methods of the model"""
 
-    employee = models.ForeignKey('Employee', on_delete=models.CASCADE)
+    employee = models.ForeignKey('Employee', on_delete=models.CASCADE, unique=True)
     start_date = models.DateField(default=date.today)
     end_date = models.DateField(default=date.today)
 
